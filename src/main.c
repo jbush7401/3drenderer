dff095ea03dc3a287d09219146c6aeb50ef5f28e
jbush7401 <jbush6@gmail.com>
2026-03-20 14:54:57 -0600

    Implement SDL initialization and window creation in main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <SDL3/SDL.h>
 
 int main(void) {
-    printf("Hello, World!\n");
-    //printf("This project uses the C17 language standard version.\n");
+    // Initialize SDL
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        fprintf(stderr, "Failed to initialize SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("3d Renderer", 800, 600, 0);
+
+    if (!window) {
+        fprintf(stderr, "Failed to create window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Event event;
+    int running = 1; 
+    while(running){
+        while(SDL_PollEvent(&event)){
+            if(event.type == SDL_EVENT_QUIT){
+                running = 0;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
